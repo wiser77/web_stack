6a0f693e0ddb853aa6636d676db5a6128f5b62c4
wiser77 <r72957193@gmail.com>
2025-06-03 13:28:08 +0530

    Update create_database.sql

--- food_ordering/sql/create_database.sql
+++ food_ordering/sql/create_database.sql
@@ -1,1 +1,19 @@
--- SQL script to create database and tables
+CREATE DATABASE food_ordering;
+
+CREATE TABLE countries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country_id INT,
+    name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+);
+
+INSERT INTO countries (name) VALUES ('India'), ('USA');
+
+INSERT INTO states (country_id, name) VALUES 
+(1, 'Tamil Nadu'), (1, 'Maharashtra'), 
+(2, 'California'), (2, 'Texas');
